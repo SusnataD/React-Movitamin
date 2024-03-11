1d34e167ef55bc20051c7184a8171cc28c9785e3
Susnata Dutta <susnata.dutta@indusnet.co.in>
2024-03-11 10:59:37 +0530

    testing commits

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const fetchData = () => {
     fetchDataFromAPi("/discover/movie").then((res) => console.log(res));
   };
-  return <div className="app">App</div>;
+  return <div className="app">App Testing</div>;
 }
 
 export default App;
